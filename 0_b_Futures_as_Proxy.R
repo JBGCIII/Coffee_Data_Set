1b22e1e9b16fecfa9b63f9ec29874aba8f6a935b
JBC <joci1358@student.su.se>
2025-06-14 14:02:11 +0200

    Added data set for coffe from CEPEA. Will only be used to explore how well the futures can be used as a proxy for them

---
+++ 0_b_Futures_as_Proxy.R
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+# The price of futures adjusted for USD/60KG shows to follow the spot price as well. However the contango
+# might be accounted for in order to create a better model as spot price cannot be used.
+

2fc643cab02b6f5b0618639abfeb3f7109473dff
JBC <joci1358@student.su.se>
2025-07-21 11:44:08 +0200

    Adde Chiapas

--- 3_Weather_Data.R
+++ 3_Weather_Data.R
@@ -21,48 +21,53 @@ dir.create("Raw_Data/Weather_Data", recursive = TRUE, showWarnings = FALSE)
 # 4. Define all regions and locations
 
 location_brazil <- list(
-  Brazil_Minas_Gerais_Patrocinio = c(lon = -46.9923, lat = -18.9349),
-  Brazil_Minas_Gerais_Varginha = c(lon = -45.4300, lat = -21.5500),
-  Brazil_Minas_Gerais_Manhuacu = c(lon = -46.1628, lat = -20.2579),
-  Brazil_Minas_Gerais_Aqua_Boa = c(lon = -42.3906, lat = -17.9910),
-  Brazil_Sao_Paulo_Campinas = c(lon = -47.0579, lat = -22.7884),
-  Brazil_Sao_Paulo_Sao_Jose = c(lon = -47.5833, lat = -20.5333),
-  Brazil_Espirito_Santo_Afonso = c(lon = -40.9743, lat = -20.0421),
-  Brazil_Espirito_Santo_Conceicao = c(lon = -41.2502, lat = -20.3633)
+  Brazil_Minas_Gerais_Patrocinio = c(lon = -46.9923, lat = -18.9349), #1
+  Brazil_Minas_Gerais_Varginha = c(lon = -45.4300, lat = -21.5500), #2
+  Brazil_Minas_Gerais_Manhuacu = c(lon = -46.1628, lat = -20.2579), #3
+  Brazil_Minas_Gerais_Aqua_Boa = c(lon = -42.3906, lat = -17.9910), #4
+  Brazil_Sao_Paulo_Campinas = c(lon = -47.0579, lat = -22.7884), #5
+  Brazil_Sao_Paulo_Sao_Jose = c(lon = -47.5833, lat = -20.5333), #6
+  Brazil_Espirito_Santo_Afonso = c(lon = -40.9743, lat = -20.0421), #7
+  Brazil_Espirito_Santo_Conceicao = c(lon = -41.2502, lat = -20.3633) #8
 )
 
 location_colombia <- list(
-  Colombia_Morelia = c(lon = -75.6736, lat = 1.3799),
-  Colombia_Andes = c(lon = -75.8469, lat = 5.6959),
-  Colombia_Santuario = c(lon = -75.9548, lat = 5.0608),
-  Colombia_Timbio = c(lon = -76.6960, lat = 2.3433)
+  Colombia_Morelia = c(lon = -75.6736, lat = 1.3799), #9
+  Colombia_Andes = c(lon = -75.8469, lat = 5.6959), #10
+  Colombia_Santuario = c(lon = -75.9548, lat = 5.0608), #11
+  Colombia_Timbio = c(lon = -76.6960, lat = 2.3433) #12
 )
 
 location_ethiopia <- list(
-  Ethiopia_Limu_Kosa = c(lon = 36.8249, lat = 7.9320),
-  Ethiopia_Bare = c(lon = 35.2119, lat = 8.2428),
-  Ethiopia_Aleto1 = c(lon = 38.4061, lat = 6.5759),
-  Ethiopia_Aleto2 = c(lon = 35.3337, lat = 6.8553)
+  Ethiopia_Limu_Kosa = c(lon = 36.8249, lat = 7.9320), #13
+  Ethiopia_Bare = c(lon = 35.2119, lat = 8.2428), #14
+  Ethiopia_Aleto1 = c(lon = 38.4061, lat = 6.5759), #15
+  Ethiopia_Aleto2 = c(lon = 35.3337, lat = 6.8553) #16
 )
 
 location_honduras <- list(
-  Honduras_Campamento = c(lon = -86.6347, lat = 14.5579),
-  Honduras_Danli = c(lon = -86.2882, lat = 13.9339),
-  Honduras_San_Jose = c(lon = -87.9285, lat = 14.2203)
+  Honduras_Campamento = c(lon = -86.6347, lat = 14.5579), #17
+  Honduras_Danli = c(lon = -86.2882, lat = 13.9339), #18
+  Honduras_San_Jose = c(lon = -87.9285, lat = 14.2203) #19
 )
 
 location_guatemala <- list(
-  Guatemala_San_Marcos = c(lon = -92.0892, lat = 15.1130),
-  Guatemala_Huehuetenango = c(lon = -91.7718, lat = 15.5633),
-  Guatemala_Jalapa = c(lon = -89.9874, lat = 14.7118)
+  Guatemala_San_Marcos = c(lon = -92.0892, lat = 15.1130), #20
+  Guatemala_Huehuetenango = c(lon = -91.7718, lat = 15.5633), #21
+  Guatemala_Jalapa = c(lon = -89.9874, lat = 14.7118) #22
 )
 
 location_peru <- list(
-  Peru_Jaen = c(lon = -78.7938, lat = -5.6981)
+  Peru_Jaen = c(lon = -78.7938, lat = -5.6981) #23
+)
+
+
+location_mexico <- list(
+  Mexico_Chiapas = c(lon = -92.3310 , lat = 15.1764) #24
 )
 
 location_china <- list(
-  China_Puer = c(lon = 100.9535, lat = 22.6555)
+  China_Puer = c(lon = 100.9535, lat = 22.6555) #24
 )
 
 # 5. Combine all locations
@@ -73,7 +78,8 @@ locations <- c(
   location_honduras,
   location_guatemala,
   location_peru,
-  location_china
+  location_china,
+  location_mexico
 )
 
 # 6. Initialize lists
